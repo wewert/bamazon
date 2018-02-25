5dc1c3b1977204fc4fa63e3bc98583e8b5b191bb
Ken Lee <wewert@gmail.com>
2018-02-24 23:43:26 -0700

    adding readme file

---
+++ README.md
@@ -0,0 +1,16 @@
+Bamazon
+==============
+
+Description
+--------------
+Created CLI application that is an Amazon-like storefront with the MySQL. The app will take orders and deplete stock from the store's inventory.
+
+*** Packages To Install ***
+inquirer package
+mysql package
+
+*** Tools ***
+mysql Workbench to create bamazon database where you will build the products table.
+
+*** Run Through ***
+Once database is created and files are installed locally, users can run app by typing 'node bamazonCustomer.js' in terminal. The app will first show all products in the database and corresponding details such as item_id, product_name, department_name, price, and stock_quantity. Users are then asked to input an item_id of a widget they want to purchase and the amount of widgets. The app will either give the user an error message via console log stating the number of widgets wanted is too high if number is above what is in stock_quantity or a console message stating the order has been fulfilled along with the total price.
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,7 +3,7 @@ var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 8080,
+  port: 3306,
   user: "root",
   password: "unlockme",
   database: "bamazon_db"
@@ -12,5 +12,66 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  connection.end();
+  console.log("=====================================================");
+  // connection.end();
+  queryAllProducts();
 });
+
+function queryAllProducts() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    for(var i = 0; i < res.length; i++) {
+      console.log("Product ID: " + res[i].item_id + " | Product Name: " + res[i].product_name + " | Price: " + res[i].price);
+    }
+    console.log("=====================================================");
+    start();
+  });
+}
+
+function start() {
+  function validateInteger(input)
+    {
+       var reg = /^\d+$/;
+       return reg.test(input) || "Inputs needs to be a positive number only!";
+    }
+  inquirer
+      .prompt([
+        {
+          message: "Please select item id of the widget you want: ",
+          type: "input",
+          name: "item_id",
+          validate: validateInteger
+        },
+        {
+          message: "Please enter the number of widgets you want: ",
+          type: "input",
+          name: "userOrderAmount",
+          validate: validateInteger
+        }
+      ])
+      .then(function(input) {
+        connection.query("SELECT * FROM products WHERE ?", {item_id: input.item_id}, function(err, results) {
+          if (err) throw err;
+          if (results.length === 0) {
+            console.log("Item Id you select does not exist...");
+            Start();
+          } else {
+            var product = results[0];
+              if (input.userOrderAmount <= product.stock_quantity) {
+                console.log("Your order has been placed...");
+
+                var adjustStock = "UPDATE products SET stock_quantity = " + (product.stock_quantity - input.userOrderAmount) + ' WHERE item_id = ' + input.item_id;
+
+                connection.query(adjustStock, function(err, results) {
+                  if (err) throw err;
+                  console.log("Your total is $" + product.price * input.userOrderAmount);
+                  console.log(product.price);
+                  connection.end();
+                })
+              } else {
+                console.log("You ordered more than we have. Please try reordering less...");
+                start();
+          }
+        }
+      })
+    })
+  }
